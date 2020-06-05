b3a087d3465c1a3e0647fd2d2cc99ae1786f16b5
MichaelLy1811 <michaelvt1998@gmail.com>
2020-06-05 10:12:10 +0700

    project initialized

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      show: true
+    };
+  }
+
+  deleteCar = () => {
+    this.setState({ show: false });
+  }
+
+  render() {
+    let myCar;
+
+    if (this.state.show) {
+      myCar = <Car />;
+    };
+
+    return(
+    <div>
+      <h1>My Garage</h1>
+      {myCar}
+      <button type="button" onClick={this.deleteCar}>Delete Car</button>
+    </div>
+    )
+  }
+}
+
+class Car extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      brand: " Ferrari ",
+      model: " LP 560-4 ",
+      color: " red ",
+      year: " 2020 "
+    };
+  }
+
+  render() {
+    return(
+    <p>
+      This is a {this.state.year}
+      {this.state.color}
+      {this.state.brand}
+      {this.state.model}
+    </p>
+    );
+  }
+}
+
+export default App;
